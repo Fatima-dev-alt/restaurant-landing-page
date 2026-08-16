cbe489e3736241f0e8f552c1abaa7a320bede7c8
Fatima-dev-alt <fatima3098657@gmail.com>
2026-08-16 11:25:06 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
     { name: 'Ember Grilled Ribeye', desc: '28-day aged, chimichurri, embers', price: '$34', rating: '★★★★★', image:'assets/images/behrouz-sasani-RZoNMrC13KU-unsplash.jpg',category: 'dinner' },
     { name: 'Crispy Chicken Bao', desc: 'Sesame slaw, chili glaze', price: '$11', rating: '★★★★☆', image:'assets/images/sandie-clarke-MGUQUgl55ik-unsplash.jpg',category: 'fastfood' },
     { name: 'Stone Fruit Galette', desc: 'Flaky crust, vanilla mascarpone', price: '$9', rating: '★★★★★', image:'assets/images/katya-azimova-5CIbULQHVBk-unsplash.jpg',category: 'desserts' },
-    { name: 'Morning Shakshuka', desc: 'Slow-cooked peppers, feta, herb oil', price: '$13', rating: '★★★★☆', icon: '🍳', category: 'breakfast' },
+    { name: 'Morning Shakshuka', desc: 'Slow-cooked peppers, feta, herb oil', price: '$13', rating: '★★★★☆', image:'assets/images/filipp-romanovski-EiqntyvJoJs-unsplash.jpg',category: 'breakfast' },
     { name: 'Loaded Fire Fries', desc: 'Smoked cheddar, jalapeño, aioli', price: '$8', rating: '★★★★☆', icon: '🍟', category: 'fastfood' },
     { name: 'Dark Chocolate Torte', desc: 'Espresso crumb, sea salt caramel', price: '$10', rating: '★★★★★', icon: '🍫', category: 'desserts' },
   ];
